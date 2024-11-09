5d2d8ca7934bc0be1716b2a4a7043e30506401b6
Nurdoolot0 <klopasd34@gmail.com>
2024-11-09 23:38:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lovecalculator/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.lovecalculator
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("getPercentage")
+    fun getPercentage(
+        @Query("fname") firstName: String,
+        @Query("sname") secondName: String,
+        @Header("x-rapidapi-key") key: String,
+        @Header("x-rapidapi-host") host: String
+    ): retrofit2.Call<LoveModel>
+}
---
+++ app/src/main/java/com/example/lovecalculator/ResultFragment.kt
@@ -0,0 +1,42 @@
+package com.example.lovecalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.lovecalculator.databinding.FragmentResultBinding
+
+class ResultFragment : Fragment() {
+
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = ResultFragmentArgs.fromBundle(requireArguments())
+        binding.tvNames.text = "${args.firstName} ❤ ${args.secondName}"
+        binding.tvPercentage.text = "${args.percentage}%"
+        binding.tvResultComment.text = args.result
+
+        binding.btnTryAgain.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
